69b57efdbcf2799e0d825a09c187a853937fa186
LeoooY <leoy.dev@gmail.com>
2020-11-18 17:21:20 +0800

    tokenizer

--- the-super-tiny-compiler.cpp
+++ the-super-tiny-compiler.cpp
@@ -1,14 +1,20 @@
 #include <string>
 #include <vector>
-
+#include <regex>
+#include <stdexcept>
 
 using std::string;
 using std::vector;
+using std::regex_match;
+using std::regex;
+using std::runtime_error;
+
 
 class Token {
 public:
     string type;
-    char value;
+    string value;
+    Token(string t, string v) :type(t), value(v) {}
 };
 
 class AST {
@@ -24,9 +30,70 @@ vector<Token> tokenizer(string input) {
     int current = 0;
     vector<Token> tokens;
     while (current < input.size()) {
-        // char curr_char = input[current];
-        ++current;
+        string curr_char = string(1, input[current]);
+
+        if (curr_char == "(") {
+            tokens.push_back(Token("paren", "("));
+            ++current;
+            continue;
+        }
+
+        if (curr_char == ")") {
+            tokens.push_back(Token("paren", ")"));
+            ++current;
+            continue;
+        }
+
+        /* WHITESPACE  */
+        if (regex_match(curr_char, regex("\s"))) {
+            current++;
+            continue;
+        }
+
+        /* NUMBERS  */
+        if (regex_match(curr_char, regex("[0-9]"))) {
+            string value;
+            while (regex_match(curr_char, regex("[0-9]"))) {
+                value += curr_char;
+                curr_char = input[++current];
+            }
+
+            tokens.push_back(Token("number", value));
+            continue;
+        }
+
+        /* STRINGS */
+        if (curr_char == "\"") {
+            string value;
+            curr_char = input[++current];
+
+            while (curr_char != "\"") {
+                value += curr_char;
+                curr_char = input[++current];
+            }
+
+            curr_char = input[++current];
+            tokens.push_back(Token("string", value));
+            continue;
+        }
+
+        /* LETTERS */
+        if (regex_match(curr_char, regex("[a-z]"))) {
+            string value;
+            curr_char = input[++current];
+
+            while (regex_match(curr_char, regex("[a-z]"))) {
+                value += curr_char;
+                curr_char = input[++current];
+            }
+
+            tokens.push_back(Token("name", value));
+            continue;
+        }
+
+        throw runtime_error("I dont know what this character is: " + curr_char);
     }
+    return tokens;
 }
 
 
